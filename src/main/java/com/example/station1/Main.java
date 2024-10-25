3a1993cc0d597c7b8d1d66d240c2bf0a9c28a377
MayukoAsanuma <asanuma_mayuko@techbowl.co.jp>
2024-10-26 01:31:28 +0900

    テスト成功

--- src/main/java/com/example/station1/Main.java
+++ src/main/java/com/example/station1/Main.java
@@ -3,7 +3,7 @@ package com.example.station1;
 public class Main{
     public static void main(String[] args) {
         // ここから
-        system.out.printin("");
+        System.out.println("Hello TechTrain!");
         // ここまで
     }
 }
